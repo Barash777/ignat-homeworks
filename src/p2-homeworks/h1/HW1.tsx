245f76b7602d93c57d4f709c88e208aecfeb59c5
Barash777 <barash.vertihvost@gmail.com>
2022-05-31 15:08:17 +0300

    homework 1

--- src/p2-homeworks/h1/HW1.tsx
+++ src/p2-homeworks/h1/HW1.tsx
@@ -1,10 +1,11 @@
 import React from 'react'
+import Message from './Message';
 
 const messageData = {
     avatar: 'https://sun9-74.userapi.com/Ph-WiuOtF985il9AvN9JqiCWedmHtSGSSTXrSA/ltEB2Z2-YO4.jpg',
-    name: 'Some Name',
-    message: 'some text',
-    time: '22:00',
+    name: 'Ahegao',
+    message: 'Text me something :)',
+    time: '22:03',
 }
 
 function HW1() {
@@ -15,12 +16,15 @@ function HW1() {
 
             {/*should work (должно работать)*/}
 
-            {/*<Message*/}
-            {/*    avatar={messageData.avatar}*/}
-            {/*    name={messageData.name}*/}
-            {/*    message={messageData.message}*/}
-            {/*    time={messageData.time}*/}
-            {/*/>*/}
+            <Message
+                avatar={messageData.avatar}
+                name={messageData.name}
+                message={messageData.message}
+                time={messageData.time}
+            />
+
+            <img
+                src={'https://production-it-incubator.s3.eu-central-1.amazonaws.com/personal-page-front-public%3Ascreen/Image/a5ba6c68-6d4f-45ab-9557-52367cf1b438_image.png'}/>
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
--- src/p2-homeworks/h1/Message.tsx
+++ src/p2-homeworks/h1/Message.tsx
@@ -1,11 +1,60 @@
 import React from 'react'
+import css from './Message.module.css'
 
-function Message() {
+type MessagePropsType = {
+    avatar: string
+    name: string
+    message: string
+    time: string
+}
+
+function Message(props: MessagePropsType) {
     return (
-        <div>
+        <>
+            <div className={css.message}>
+                <img src={props.avatar}/>
+                <div className={css.messageData}>
+                    <div className={css.name}>
+                        <span>{props.name}</span>
+                    </div>
+                    <p><span className={css.text}>{props.message}</span>
+                        <span className={css.time}>{props.time}</span></p>
+                    {/*<span className={css.timeRight}>{props.time}</span>*/}
+                </div>
+                <div className={css.corner}>
+                    <div className={css.circle}></div>
+                </div>
+            </div>
+        </>
 
-        </div>
+
     )
 }
+
+/*
+<div className={css.message}>
+    <img src={props.avatar}/>
+    {/!*<p>{props.name}</p>*!/}
+    <p>{props.message} <span className={css.timeRight}>{props.time}</span></p>
+    {/!*<span className={css.timeRight}>{props.time}</span>*!/}
+</div>
+*/
+
+/*<li className="ks-item ks-self"><span className="ks-avatar ks-offline"> <img
+    src="https://bootdey.com/img/Content/avatar/avatar4.png" className="rounded-circle" width="36" height="36"> </span>
+    <div className="ks-body">
+        <div className="ks-header"><span className="ks-name">Brian Diaz</span> <span
+            className="ks-datetime">6:46 PM</span></div>
+        <div className="ks-message">The weird future of movie theater food</div>
+    </div>
+</li>*/
+
+
+/*<div className="container">
+            <img src="/w3images/bandmember.jpg" alt="Avatar">
+                <p>Hello. How are you today?</p>
+                <span className="time-right">11:00</span>
+        </div>*/
+
 
 export default Message
