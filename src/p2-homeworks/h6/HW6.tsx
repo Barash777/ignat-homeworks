7527c51749e83beb2a9bc83b43c07f37760a50eb
Barash777 <barash.vertihvost@gmail.com>
2022-07-01 12:08:30 +0300

    homework 6

--- src/p2-homeworks/h6/HW6.tsx
+++ src/p2-homeworks/h6/HW6.tsx
@@ -2,15 +2,22 @@ import React, {useState} from 'react'
 import SuperEditableSpan from './common/c4-SuperEditableSpan/SuperEditableSpan'
 import SuperButton from '../h4/common/c2-SuperButton/SuperButton'
 import {restoreState, saveState} from './localStorage/localStorage'
+import css from './HW6.module.css'
+
+
+const editableSpanLSKey = 'editable-span-value';
 
 function HW6() {
+    // default
     const [value, setValue] = useState<string>('')
+    // with value from localStorage
+    // const [value, setValue] = useState<string>(restoreState<string>(editableSpanLSKey, ''))
 
     const save = () => {
-        saveState<string>('editable-span-value', value)
+        saveState<string>(editableSpanLSKey, value)
     }
     const restore = () => {
-        // setValue()
+        setValue(restoreState<string>(editableSpanLSKey, value))
     }
 
     return (
@@ -19,7 +26,7 @@ function HW6() {
             homeworks 6
 
             {/*should work (должно работать)*/}
-            <div>
+            <div className={css.forSpan}>
                 <SuperEditableSpan
                     value={value}
                     onChangeText={setValue}
