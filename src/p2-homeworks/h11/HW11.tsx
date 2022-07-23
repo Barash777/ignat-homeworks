45d9025a9ba6be0af901d5a6fd5bd891fe2d5796
Barash777 <barash.vertihvost@gmail.com>
2022-07-23 20:00:54 +0300

    homework 11

--- src/p2-homeworks/h11/HW11.tsx
+++ src/p2-homeworks/h11/HW11.tsx
@@ -1,10 +1,19 @@
 import React, {useState} from 'react'
 import SuperRange from './common/c7-SuperRange/SuperRange'
 import SuperDoubleRange from './common/c8-SuperDoubleRange/SuperDoubleRange'
+import css from './HW11.module.css'
 
 function HW11() {
-    const [value1, setValue1] = useState(0)
-    const [value2, setValue2] = useState(100)
+    const [value1, setValue1] = useState<number>(50)
+    const [value2, setValue2] = useState<number>(100)
+    const min = 0, max = 100;
+
+    const onChangeDoubleRangeHandler = (value: [number, number]) => {
+        if (value1 !== value[0])
+            setValue1(value[0])
+        if (value2 !== value[1])
+            setValue2(value[1])
+    }
 
     return (
         <div>
@@ -12,20 +21,39 @@ function HW11() {
             homeworks 11
 
             {/*should work (должно работать)*/}
-            <div>
-                <span>{value1}</span>
-                <SuperRange
-                    // сделать так чтоб value1 изменялось
-                />
+            <div className={css.wrapper}>
+                <div>
+                    <span className={css.minValue}>{value1}</span>
+                    <SuperRange
+                        onChangeRange={setValue1}
+                        value={value1}
+                        min={min}
+                        max={max}
+                        // сделать так чтоб value1 изменялось
+                    />
+                </div>
+
+                <div>
+                    <span className={css.minValue}>{value1}</span>
+                    <SuperDoubleRange
+                        min={min}
+                        max={max}
+                        value={[value1, value2]}
+                        onChangeRange={onChangeDoubleRangeHandler}
+
+                        // сделать так чтоб value1 и value2 изменялось
+                    />
+                    <span>{value2}</span>
+                </div>
             </div>
 
-            <div>
-                <span>{value1}</span>
-                <SuperDoubleRange
-                    // сделать так чтоб value1 и value2 изменялось
+            {/*<div>
+                <MultiRangeSlider
+                    min={0}
+                    max={1000}
+                    onChange={({min, max}) => console.log(`min = ${min}, max = ${max}`)}
                 />
-                <span>{value2}</span>
-            </div>
+            </div>*/}
 
             <hr/>
             {/*для личного творчества, могу проверить*/}
--- src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
+++ src/p2-homeworks/h11/common/c8-SuperDoubleRange/SuperDoubleRange.tsx
@@ -1,22 +1,96 @@
-import React from 'react'
+import React, {ChangeEvent} from 'react'
+import css from './SuperDoubleRange.module.css'
+
+// тип пропсов обычного инпута
+// type DefaultInputPropsType = DetailedHTMLProps<InputHTMLAttributes<HTMLInputElement>, HTMLInputElement>
 
 type SuperDoubleRangePropsType = {
     onChangeRange?: (value: [number, number]) => void
     value?: [number, number]
-    // min, max, step, disable, ...
+    min?: number
+    max?: number
+    step?: number
+    disabled?: boolean
+    className?: string
 }
 
 const SuperDoubleRange: React.FC<SuperDoubleRangePropsType> = (
     {
-        onChangeRange, value,
-        // min, max, step, disable, ...
+        min,
+        max,
+        step,
+        value,
+        disabled,
+        onChangeRange,
+        className
     }
 ) => {
     // сделать самому, можно подключать библиотеки
 
+    const defMin = 0, defMax = 100, defStep = 1;
+
+    const onChangeCallback = (e: ChangeEvent<HTMLInputElement>) => {
+        if (onChangeRange) {
+            // define MIN or MAX was changed
+            // console.log(e.target.id)
+            const id = e.target.id
+            if (id === 'rangeMin') {
+                let minValue = +e.currentTarget.value
+                const maxValue = value ? value[1] : (max ?? defMax)
+                if (minValue > maxValue) {
+                    minValue = maxValue
+                }
+                onChangeRange([minValue, maxValue])
+            } else if (id === 'rangeMax') {
+                const minValue = value ? value[0] : (min ?? defMin)
+                let maxValue = +e.currentTarget.value
+                if (maxValue < minValue) {
+                    maxValue = minValue
+                }
+                onChangeRange([minValue, maxValue])
+            }
+        }
+    }
+
+    const finalRangeMinClassName = `${css.range} ${css.rangeMin} ${className ? className : ''}`
+    const finalRangeMaxClassName = `${css.range} ${css.rangeMax} ${className ? className : ''}`
+
     return (
         <>
-            DoubleRange
+            {/*<div className={css.wrap} role="group" aria-labelledby="multi-lbl">
+                <div id="multi-lbl">Multi thumb slider:</div>
+                <label className={css.srOnly} htmlFor="a">Value A:</label>
+                <input id="a" type="range" min="-50" value="-30" max="50"/>
+                <label className={css.srOnly} htmlFor="b">Value B:</label>
+                <input id="b" type="range" min="-50" value="20" max="50"/>
+            </div>*/}
+
+            <div className={css.inline}>
+                <div className={css.wrap}>
+                    <input
+                        id={'rangeMin'}
+                        className={finalRangeMinClassName}
+                        type={'range'}
+                        min={min ?? defMin}
+                        max={max ?? defMax}
+                        step={step ?? defStep}
+                        value={value ? value[0] : undefined}
+                        onChange={onChangeCallback}
+                        disabled={disabled}
+                    />
+                    <input
+                        id={'rangeMax'}
+                        className={finalRangeMaxClassName}
+                        type={'range'}
+                        min={min ?? defMin}
+                        max={max ?? defMax}
+                        step={step ?? defStep}
+                        value={value ? value[1] : undefined}
+                        onChange={onChangeCallback}
+                        disabled={disabled}
+                    />
+                </div>
+            </div>
         </>
     )
 }
